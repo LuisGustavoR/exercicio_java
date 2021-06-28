764f60420c6140b998165e109b780221ed226687
Luis Gustavo <ll.gustavo2014@gmail.com>
2021-06-27 21:16:48 -0300

    Criação das classes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # exercicio_java
+
+Exercicio em Java.
  
---
+++ exercicio/src/exerciciojava/Vetor.java
@@ -0,0 +1,10 @@
+package exerciciojava;
+
+public class Vetor {
+
+	public static void main(String[] args) {
+		// TODO Stub de método gerado automaticamente
+
+	}
+
+}
